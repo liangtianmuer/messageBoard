8b3573f8cd6136a940d4252dd485d23a23d54d7f
liangtianmuer <liangtianmuer@163.com>
2019-07-08 09:46:54 +0800

    First commit.

---
+++ middleware/validate.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function parseField(field) {
+  return field
+    .split(/\[|\]/)
+    .filter((s) => s);
+}
+function getField(req, field) {
+  let val = req.body;
+  field.forEach((prop) => {
+    val = val[prop];
+  });
+  return val;
+}
+exports.required = (field) => {
+  field = parseField(field);
+  return (req, res, next) => {
+    if (getField(req, field)) {
+      next();
+    } else {
+      res.error(`${field.join(' ')} is required`);
+      //console.log(`${field.join(' ')} is required`);
+      //res.end(`${field.join(' ')} is required`);
+      res.redirect('back');
+    }
+  }
+};
+exports.lengthAbove = (field, len) => {
+  field = parseField(field);
+  return (req, res, next) => {
+    if (getField(req, field).length > len) {
+      next();
+    } else {
+      res.error(`${field.join(' ')} must have more than ${len} characters`);
+      res.redirect('back');
+      //console.log(`${field.join(' ')} must have more than ${len} characters`);
+      //res.end(`${field.join(' ')} must have more than ${len} characters`);
+    }
+  }
+};
